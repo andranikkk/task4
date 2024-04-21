0cf4e4aa971922961a500d87146e462ea2cdfaa3
Andranik Aivazyan <ayvazyanandranik46@gmail.com>
2024-04-21 18:36:11 +0400

    comm1

--- controllers/users.js
+++ controllers/users.js
@@ -92,64 +92,9 @@ const all = async (req, res) => {
   }
 };
 
-const remove = async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    await prisma.user.delete({
-      where: { id },
-    });
-    res.status(204).json("User deleted!");
-  } catch (error) {
-    res.status(500).json({ message: "Error deleting user" });
-    console.error(error);
-  }
-};
-
-const block = async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    await prisma.user.updateMany({
-      where: {
-        id,
-      },
-      data: {
-        isBlocked: true,
-      },
-    });
-    res.status(204).json({ message: "Ok" });
-  } catch (error) {
-    res.status(500).json({ message: "Error processing" });
-    console.error(error);
-  }
-};
-
-const unblock = async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    await prisma.user.updateMany({
-      where: {
-        id,
-      },
-      data: {
-        isBlocked: false,
-      },
-    });
-    res.status(204).json("Ok");
-  } catch (error) {
-    res.status(500).json({ message: "Error processing" });
-    console.error(error);
-  }
-};
-
 module.exports = {
   login,
   register,
   current,
   all,
-  remove,
-  block,
-  unblock,
 };
